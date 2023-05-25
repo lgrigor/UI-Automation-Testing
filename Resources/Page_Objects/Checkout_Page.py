2132276cfaae0223c7bceb1de4db13dffc3c6131
lgrigor <levon.grigoryan97@gmail.com>
2023-05-25 17:23:03 +0400

    Corrected some functions.

--- Resources/Page_Objects/Checkout_Page.py
+++ Resources/Page_Objects/Checkout_Page.py
@@ -42,35 +42,35 @@ class CheckoutPage(CommonPage):
         self.input_text(locator=self.FIRST_NAME_FIELD, text=name)
 
     def input_last_name(self, name: str):
-        self.input_text(locator=self.FIRST_NAME_FIELD, text=name)
+        self.input_text(locator=self.LAST_NAME_FIELD, text=name)
 
     def input_postal_code(self, code: str or int):
-        self.input_text(locator=self.FIRST_NAME_FIELD, text=str(code))
+        self.input_text(locator=self.POSTAL_CODE_FIELD, text=str(code))
 
     def verify_first_name_error_message_is_visible(self, timeout: int):
-        self.verify_error_message_is_visible(locator=self.ERROR_MESSAGE_CONTAINER,
-                                             expected_error_message=self.FIRST_NAME_ERROR_MESSAGE,
-                                             timeout=timeout)
+        return self.verify_error_message_is_visible(locator=self.ERROR_MESSAGE_CONTAINER,
+                                                    expected_error_message=self.FIRST_NAME_ERROR_MESSAGE,
+                                                    timeout=timeout)
 
     def verify_last_name_error_message_is_visible(self, timeout: int):
-        self.verify_error_message_is_visible(locator=self.ERROR_MESSAGE_CONTAINER,
-                                             expected_error_message=self.LAST_NAME_ERROR_MESSAGE,
-                                             timeout=timeout)
+        return self.verify_error_message_is_visible(locator=self.ERROR_MESSAGE_CONTAINER,
+                                                    expected_error_message=self.LAST_NAME_ERROR_MESSAGE,
+                                                    timeout=timeout)
 
     def verify_postal_code_error_message_is_visible(self, timeout: int):
-        self.verify_error_message_is_visible(locator=self.ERROR_MESSAGE_CONTAINER,
-                                             expected_error_message=self.POSTAL_CODE_ERROR_MESSAGE,
-                                             timeout=timeout)
+        return self.verify_error_message_is_visible(locator=self.ERROR_MESSAGE_CONTAINER,
+                                                    expected_error_message=self.POSTAL_CODE_ERROR_MESSAGE,
+                                                    timeout=timeout)
 
     def verify_order_complete_message_is_visible(self, timeout: int):
-        self.verify_error_message_is_visible(locator=self.ORDER_COMPLETE_HEADER,
-                                             expected_error_message=self.ORDER_COMPLETE_MESSAGE,
-                                             timeout=timeout)
+        return self.verify_error_message_is_visible(locator=self.ORDER_COMPLETE_HEADER,
+                                                    expected_error_message=self.ORDER_COMPLETE_MESSAGE,
+                                                    timeout=timeout)
 
     def verify_item_total(self, expected_price: float, timeout: int):
         item_total_label = self.wait_until_element_visible(locator=self.ITEM_TOTAL_LABEL, timeout=timeout)
         if bool(item_total_label):
-            numbers = re.search(r"\d+\.\d+$", item_total_label)
+            numbers = re.search(r"\d+\.\d+$", item_total_label.text)
             if numbers:
                 return float(numbers.group()) == expected_price
             else:
@@ -81,7 +81,7 @@ class CheckoutPage(CommonPage):
     def verify_tax(self, expected_tax: float, timeout: int):
         tax_label = self.wait_until_element_visible(locator=self.TAX_LABEL, timeout=timeout)
         if bool(tax_label):
-            numbers = re.search(r"\d+\.\d+$", tax_label)
+            numbers = re.search(r"\d+\.\d+$", tax_label.text)
             if numbers:
                 return float(numbers.group()) == expected_tax
             else:
@@ -92,7 +92,7 @@ class CheckoutPage(CommonPage):
     def verify_total(self, expected_total: float, timeout: int):
         total_label = self.wait_until_element_visible(locator=self.TOTAL_LABEL, timeout=timeout)
         if bool(total_label):
-            numbers = re.search(r"\d+\.\d+$", total_label)
+            numbers = re.search(r"\d+\.\d+$", total_label.text)
             if numbers:
                 return float(numbers.group()) == expected_total
             else:
@@ -102,4 +102,4 @@ class CheckoutPage(CommonPage):
 
     @staticmethod
     def calculate_tax(price: float, percentage=8):
-        return price * percentage / 100
+        return float("%.2f" % (price * percentage / 100))
